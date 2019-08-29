948b3c1a8366e5d08876174a35476ad59d558ba4
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-08-29 18:36:56 -0400

    front end app set up

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navigation from './layout/Navigation'
+
+const App = () => {
+  return(
+    <Navigation />
+  );
+};  
+
+export default App;
